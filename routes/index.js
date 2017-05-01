468f8c8ec1778abaad7a9a0a85d9ce091b71e44f
V Morishima <vmorishima@gmail.com>
2017-05-01 13:00:47 -0700

    add header parser functionality

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var parser = require('ua-parser-js');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var ip = req.ip;
+  var language = req.acceptsLanguages()[0];
+  var ua = parser(req.headers['user-agent']);
+  var user = ua.os.name + ' ' + ua.os.version;
+  res.send({ipaddress: ip, language: language, software: user});
 });
 
 module.exports = router;
