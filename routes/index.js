c5ab13a671290667519cc63ee340a0b1db83f1bb
V Morishima <vmorishima@gmail.com>
2017-05-01 13:07:18 -0700

    ip address

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,10 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var ip = req.ip;
+  var ip = req.headers['x-forwarded-for'] ||
+     req.connection.remoteAddress ||
+     req.socket.remoteAddress ||
+     req.connection.socket.remoteAddress;
   var language = req.acceptsLanguages()[0];
   var ua = parser(req.headers['user-agent']);
   var user = ua.os.name + ' ' + ua.os.version;
